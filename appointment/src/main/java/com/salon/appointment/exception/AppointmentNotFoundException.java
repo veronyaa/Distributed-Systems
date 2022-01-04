179e6671c8ffc898de8a873b979156a8761efb04
Veronika <89996911+veronyaa@users.noreply.github.com>
2022-01-04 23:16:29 +0200

    Delete appointment directory

--- appointment/src/main/java/com/salon/appointment/exception/AppointmentNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.salon.appointment.exception;
-
-public class AppointmentNotFoundException  extends RuntimeException{
-}
--- appointment/src/main/java/com/salon/appointment/repo/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salon.appointment.repo;
-
-import com.salon.appointment.repo.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
-}
